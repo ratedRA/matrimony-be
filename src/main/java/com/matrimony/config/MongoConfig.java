6e947aa9979901fbf22468befc70ac844af95254
amanprasad <aman.prasad@1stdibs.com>
2021-12-28 04:00:19 +0530

    added initial setup for mongodb

---
+++ src/main/java/com/matrimony/config/MongoConfig.java
@@ -0,0 +1,34 @@
+package com.matrimony.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class MongoConfig extends AbstractMongoClientConfiguration {
+    @Override
+    protected String getDatabaseName() {
+        return "matrimony";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://ratedRa:matrimony@cluster0.1xe85.mongodb.net/matrimony?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+
+        return mongoClient;
+    }
+
+    @Override
+    protected Collection<String> getMappingBasePackages() {
+        return Collections.singleton("com.matrimony");
+    }
+}
